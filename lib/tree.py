e2470748191678ff3c882c07f9552d05a9d8eff0
Eric-Martin <gaspar.ericmartin@gmail.com>
2024-04-23 10:51:42 -0400

    Complete assignment

--- lib/tree.py
+++ lib/tree.py
@@ -3,4 +3,22 @@ class Tree:
     self.root = root
 
   def get_element_by_id(self, id):
-    pass
+    result = None
+    nodes_to_visit = [self.root]
+
+    while len(nodes_to_visit) > 0 and result == None:
+      node = nodes_to_visit.pop(0)
+
+      if node['id'] == id:
+        result = node
+      else:
+        #Depth first
+        #nodes_to_visit = node['children'] + nodes_to_visit
+
+        #Breadth first
+        nodes_to_visit = nodes_to_visit + node['children']
+    
+    return result
+
+
+
